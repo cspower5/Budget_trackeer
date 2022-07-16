90cc10f0c5caf8173e8d65f75666695f9974c65c
Chrisborer <chris.borer@gmail.com>
2022-07-16 11:35:00 -0500

    changed mongodb name back

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/chris_budget_trackeer";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
